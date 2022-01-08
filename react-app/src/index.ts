acd10aaf639bbc275bcaf5955c879abd85f789c3
Shruthi Rai <shruthi.rai@emerson.com>
2022-01-08 18:24:07 +0100

    fix: exporting files from index file

--- react-app/src/index.ts
+++ react-app/src/index.ts
@@ -1,7 +1,6 @@
-import ContinuousLegend from './component/Legend/ContinuousLegend';
-import DiscreteColorLegend from './component/Legend/DiscreteLegend';
-
-export {
-    ContinuousLegend,
-    DiscreteColorLegend
-};
+export *  from './component/Legend/ContinuousLegend';
+export * from './component/Legend/DiscreteLegend';
+export * from './component/Utils/continousLegend';
+export * from './component/ColorTableTypes';
+export * from './component/WelllayerTemplateTypes';
+export const colorTables = require('./component/color-tables');
