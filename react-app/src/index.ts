7a76126312ac11559b03c977db8c063615db77b9
Shruthi Rai <shruthi.rai@emerson.com>
2022-01-11 17:34:06 +0100

    fix: testing

--- react-app/src/index.ts
+++ react-app/src/index.ts
@@ -1,6 +1,6 @@
 export { ContinuousLegend } from './component/Legend/ContinuousLegend';
 export { colorTableData, DiscreteColorLegend } from './component/Legend/DiscreteLegend';
-export { colorsArray, rgbValues, RGBToHex} from './component/Utils/continousLegend';
 export { colorTablesObj, colorTablesArray } from './component/ColorTableTypes';
+export { colorsArray, rgbValues, RGBToHex} from './component/Utils/continousLegend';
 import * as colorTables from './component/color-tables.json';
 export default colorTables;
