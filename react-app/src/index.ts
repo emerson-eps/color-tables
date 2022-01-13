6f852994d5afcd1c9383875832d5e149f1c7c46c
Shruthi Rai <shruthi.rai@emerson.com>
2022-01-13 07:38:04 +0100

    fix:lint error

--- react-app/src/index.ts
+++ react-app/src/index.ts
@@ -1,6 +1,6 @@
 export { ContinuousLegend } from './component/Legend/ContinuousLegend';
-export { colorTableData, DiscreteColorLegend } from './component/Legend/DiscreteLegend';
 export { colorsArray, rgbValues, RGBToHex} from './component/Utils/continousLegend';
+export { colorTableData, DiscreteColorLegend } from './component/Legend/DiscreteLegend';
 export { colorTablesObj, colorTablesArray } from './component/ColorTableTypes';
 export { propertiesObj, templateArray } from './component/WelllayerTemplateTypes';
 import * as colorTables from './component/color-tables.json';
