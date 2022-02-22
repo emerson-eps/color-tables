9947ae614fd07698d39a4cd16880f72b2b06227b
Shruthi Rai <shruthi.rai@emerson.com>
2022-02-22 17:56:53 +0100

    feat: Displaying lists of continiuous legends

---
+++ react-app/src/stories/ColorLegends/ContinousLegends/Permeability.stories.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {ContinuousLegend} from "../../../component/Legend/ContinuousLegend";
+export default {
+    component: ContinuousLegend,
+    title: "Legends/ContinousLegend",
+};
+import colorTables from "../../../component/color-tables.json";
+
+const min = -999;
+const max = 14023;
+const dataObjectName = "Wells / PERM";
+const position = [16, 10];
+const horizontal = true;
+const colorName = "Permeability";
+
+const Template = (args) => {
+    return <ContinuousLegend {...args} />;
+};
+
+export const PermeabilityTemplate = Template.bind({});
+PermeabilityTemplate.args = {
+    min,
+    max,
+    dataObjectName,
+    position,
+    colorName,
+    colorTables,
+    horizontal,
+};
---
+++ react-app/src/stories/ColorLegends/ContinousLegends/PoroTot.stories.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {ContinuousLegend} from "../../../component/Legend/ContinuousLegend";
+export default {
+    component: ContinuousLegend,
+    title: "Legends/ContinousLegend",
+};
+import colorTables from "../../../component/color-tables.json";
+
+const min = 0;
+const max = 0.35;
+const dataObjectName = "Wells / POROTOT";
+const position = [16, 10];
+const horizontal = true;
+const colorName = "Porosity";
+
+const Template = (args) => {
+    return <ContinuousLegend {...args} />;
+};
+
+export const PoroTotTemplate = Template.bind({});
+PoroTotTemplate.args = {
+    min,
+    max,
+    dataObjectName,
+    position,
+    colorName,
+    colorTables,
+    horizontal,
+};
