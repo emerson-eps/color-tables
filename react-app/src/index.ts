250f270c434782334c55b94c5b727c70fc7b7e14
Shruthi Rai <shruthi.rai@emerson.com>
2022-04-26 16:02:31 +0200

    fix: change colortable import style

--- react-app/src/index.ts
+++ react-app/src/index.ts
@@ -5,8 +5,7 @@ import { ColorLegend } from './component/Legend/ColorLegend';
 import { colorsArray, rgbValues, RGBToHex } from './component/Utils/continousLegend';
 import { d3ColorScales } from './component/Utils/d3ColorScale';
 import { colorTablesObj, colorTablesArray } from './component/colorTableTypes';
-import * as colorTablesJsonData from './component/color-tables.json';
-const colorTables = colorTablesJsonData;
+import * as colorTables from './component/color-tables.json';
 
 export {
     ColorSelectorAccordion,
