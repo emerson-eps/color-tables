49c900459c74c05cd08580c50730c3aabdb5598a
t0oF <93762994+w1nklr@users.noreply.github.com>
2026-06-04 18:58:51 +0200

    chore: update storybook to version 9
    Used `npx storybook@9 upgrade`

--- react-app/.storybook/main.ts
+++ react-app/.storybook/main.ts
@@ -1,15 +1,11 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-docs"],
 
   framework: {
     name: "@storybook/react-vite",
-    options: {}
+    options: {},
   },
 
   docs: {},
@@ -20,7 +16,7 @@ const config: StorybookConfig = {
   },
 
   typescript: {
-    reactDocgen: "react-docgen-typescript"
-  }
+    reactDocgen: "react-docgen-typescript",
+  },
 };
 export default config;
